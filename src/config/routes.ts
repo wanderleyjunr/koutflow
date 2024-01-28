914d4f398396c51b5a0ae45b0c266d06222faec9
Wanderley Alino <wanderleyjunr@gmail.com>
2024-01-28 14:23:53 -0300

    modify: alter file route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -90,23 +90,11 @@ export const routes = {
   maintenance: '/maintenance',
   blank: '/blank',
   auth: {
-    signUp1: '/auth/sign-up-1',
-    signUp2: '/auth/sign-up-2',
-    signUp3: '/auth/sign-up-3',
-    signUp4: '/auth/sign-up-4',
-    signUp5: '/auth/sign-up-5',
+    signUp: '/signup',
     // sign in
-    signIn1: '/auth/sign-in-1',
-    signIn2: '/auth/sign-in-2',
-    signIn3: '/auth/sign-in-3',
-    signIn4: '/auth/sign-in-4',
-    signIn5: '/auth/sign-in-5',
+    signIn: '/signin',
     // forgot password
-    forgotPassword1: '/auth/forgot-password-1',
-    forgotPassword2: '/auth/forgot-password-2',
-    forgotPassword3: '/auth/forgot-password-3',
-    forgotPassword4: '/auth/forgot-password-4',
-    forgotPassword5: '/auth/forgot-password-5',
+    forgotPassword: '/forgotpassword',
     // OTP
     otp1: '/auth/otp-1',
     otp2: '/auth/otp-2',
